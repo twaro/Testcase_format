74e40c12626db9fa3801c0dd13e450fc4609cda3
twaro <niechcereklam@gmail.com>
2020-02-11 21:30:15 +0100

    remove remote venv

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\tester\Desktop\PycharmProjects\Testcase_format\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
